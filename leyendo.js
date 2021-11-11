6bf38f97c9265d881a4cc9928b1a7f90b6cf540c
DianaMsJz <dianamsjz@gmail.com>
2021-11-11 09:48:04 -0700

    Modelo bloqueante con Node

--- leyendo.js
+++ leyendo.js
@@ -1,8 +1,8 @@
 var fs = require('fs')
-console.log('\nAbriendo Archivo...')
+console.log('\nAbriendo Archivo ...')
 
 var content = fs.readFileSync('el_quijote.txt', 'utf8')
 console.log(content)
 
-console.log('\nHaciendo otra cosa')
+console.log('\nHaciendo otra cosa ')
 console.log(process.uptime())
